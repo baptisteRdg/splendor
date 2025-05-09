c6fbc7231b249473328ab0486376f0cb0f5199c6
Paulpen773 <paul.pensee@gmail.com>
2025-05-09 12:14:52 +0200

    feat: add a lot of card method

--- Splendor/src/model/Money.java
+++ Splendor/src/model/Money.java
@@ -21,4 +21,6 @@ public enum Money {
 	public String toString() {
 		return name;
 	}
+	
+	
 }
--- Splendor/src/model/Upgrade.java
+++ Splendor/src/model/Upgrade.java
@@ -11,7 +11,7 @@ public class Upgrade implements Card{
 	private final HashMap<Money,Integer> cost;
 	
 	
-	Upgrade(int level,String id){
+	Upgrade(int level,String id,Money money){
 		Objects.requireNonNull(id);
 		if (level>3 || level<1) {
 			throw new IllegalArgumentException("value on level is out on scope");
@@ -19,8 +19,28 @@ public class Upgrade implements Card{
 		this.level=level;
 		this.pts=level;
 		this.id=id;
-		this.advantage=new Money();
+		this.advantage= money;
 		this.cost= new HashMap<Money,Integer>();
-		
+	}
+	
+	public int point() {
+		return pts;
+	}
+	
+	public Money advantage() {
+		return advantage;
+	}
+	
+	public HashMap<Money,Integer> cost(){
+		return cost;
+	}
+	
+	public String id() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Uprgade "+level+" pts: "+pts+" advantage :"+advantage+" cost:"+cost;
 	}
 }
