8738854c6be4eb4b5c6208ef60082bee295a8d3b
baptiste-rdg <baptiste.rodriguies@edu.esiee.fr>
2025-06-10 16:27:51 +0200

    rendu

--- Splendor/src/main/Main.java
+++ Splendor/src/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+import java.util.Objects;
+
 import controller.Game;
 import controller.graphicAction;
 import view.Ter;
@@ -7,20 +9,18 @@ import view.Ter;
 public class Main {
 
 	public static void main(String[] args) {
-		var display = false;
-		if(args.length == 0) {
-			for (String arg : args) {
-	            	if(arg.equals("--text")) display = false;
-	        	}
-		}
+		Objects.requireNonNull(args);
+		var display = true;
+	    if(args[0].equals("--text")) display = false;
+	    
 		if(display) {var screen = new graphicAction(); screen.runner();}
 		else {
 			var choix = 0;
 			while(choix > 2 || choix < 1) {
 				choix = Ter.sc("\n⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n	\n\n	-:-:- Bienvenue sur Splendor -:-:-\n"
 						+ "\n\n sélectionné votre mode de jeu :"
-						+ "\n	1) Version classique	(v2)"
-						+ "\n	2) Version réduite 	(v1)"
+						+ "\n	1) Version classique"
+						+ "\n	2) Version réduite"
 						+ "\n->");
 			}
 			 
